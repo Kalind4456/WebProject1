d77eded65ea538d2e41d119e37b3664f34af1fd2
Kalind <126040927+Kalind4456@users.noreply.github.com>
2024-03-09 16:03:40 -0500

    added player names and image

---
+++ main.js
@@ -0,0 +1,10 @@
+players = [
+  {
+    name: "Jokic",
+    image: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png",
+  },
+  {
+    name: "LeBron",
+    image: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+  },
+];
