0b79c3cd4a819628b0d1a8bd89233f162fe358d9
Kalind <126040927+Kalind4456@users.noreply.github.com>
2024-03-09 19:02:52 -0500

    finished a lot of the javascript

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-const players = [
-  {
-    name: "Jokic",
-    image: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png",
-  },
-  {
-    name: "LeBron",
-    image: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
-  },
-];
+function run() {
+  let playerName;
+  let playerImage;
+  const players = [
+    {
+      name: "Jokic",
+      image: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png",
+    },
+    {
+      name: "LeBron",
+      image: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+    },
+  ];
 
-function getPlayer() {
-  return players[Math.round(Math.round(players.length))];
+  function getPlayer() {
+    const randomIndex = Math.floor(Math.random() * players.length);
+    const player = players[randomIndex];
+    playerImage = player.image;
+    playerName = player.name;
+
+    const image = document.querySelector("#playerImage");
+    image.src = playerImage;
+    image.alt = playerName;
+  }
+  getPlayer();
+
+  const textbox = document.getElementById("textbox");
+
+  const submit = document.getElementById("submitBtn");
+  submit.onclick = function () {
+    check();
+  };
+
+  function check() {
+    if (textbox.value === playerName) {
+      alert("You win!");
+    }
+  }
 }
-
-let player = getPlayer();
-const image = document.getElementById("playerImage");
-image.src = player.image;
+const restart = document.getElementById("restartBtn");
+restart.onclick = function () {
+  run();
+};
+run();
